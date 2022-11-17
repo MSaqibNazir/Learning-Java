1fc8aa3ea7d0fbfb06ad420047e7ec95399ffb04
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-11-17 09:21:03 +0500

    Assignment Operators

--- Opperators.js
+++ Opperators.js
@@ -25,3 +25,41 @@ console.log(x);
 
 // Decreament (--)
 console.log(--y);
+
+
+// Assignment Operator 
+// ===================
+
+let a = 10;
+
+(a++);
+a = a + 1 // these 2 are same 
+console.log(a);
+
+
+
+let b = 100
+
+// b = b + 50;
+b += 50;
+console.log(b);
+
+
+// b = b - 50;
+b -= 50;
+console.log(b);
+
+
+// b = b * 50;
+b *= 50;
+console.log(b);
+
+
+// b = b / 50;
+b /= 50;
+console.log(b);
+
+
+// b = b % 50;
+b %= 50;
+console.log(b);
