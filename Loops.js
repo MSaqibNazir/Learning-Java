ab765c329111a575f1926f1e5233a66119fad946
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-11 00:33:06 +0500

    while Loop & do-while loop

--- Loops.js
+++ Loops.js
@@ -6,17 +6,56 @@
 // FOR Loop
 
 for (let i = 0; i < 5; i++){
-    console.log('Hello World ', i);
-}
+    // console.log('Hello World ', i);
+};
+
 
 for (let i = 5; i > 0; i--){
-    console.log('Hello World ', i);
-}
+    // console.log('Hello World ', i);
+};
 
+// to print odd values on console
 for (let i = 0; i <= 5; i++){
-    if (i % 2 ==! 0) console.log(i);
-}
+    // if (i % 2 !== 0) console.log(i);
+};
 
+// to print odd values on console
 for (let i = 10; i > 0; i--){
-    if (i % 2 ==! 0) console.log(i);
-}
+    // if (i % 2 !== 0) console.log(i);
+};
+
+
+
+
+// WHILE Loop
+
+let i = 0;
+
+// while (Condition)
+while (i <=5){
+    // console.log('Hello While Loop', i);
+    i++
+};
+
+
+let j = 0;
+while (j <= 5){
+    // if (j % 2 !== 0) console.log('Hello While Odd Values', j)
+    j++;
+};
+
+
+
+// Do-While Loop
+let k = 0;
+do{
+    if (k % 2 !== 0) console.log('Hello do while loop' , k);
+    k++;
+} while(k <= 5);
+
+let l = 9;
+do {
+    if (l % 2 !== 0) console.log('Hello Do While Loop False', l)
+    l++;
+} while ( l <= 5);
+
