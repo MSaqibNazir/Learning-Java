977d4e88742ad143312d7bdcae4f62c06fcb2dd2
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-10 21:59:37 +0500

    For Loop

---
+++ Loops.js
@@ -0,0 +1,22 @@
+// LOOPS
+
+// ========================================================
+
+
+// FOR Loop
+
+for (let i = 0; i < 5; i++){
+    console.log('Hello World ', i);
+}
+
+for (let i = 5; i > 0; i--){
+    console.log('Hello World ', i);
+}
+
+for (let i = 0; i <= 5; i++){
+    if (i % 2 ==! 0) console.log(i);
+}
+
+for (let i = 10; i > 0; i--){
+    if (i % 2 ==! 0) console.log(i);
+}
