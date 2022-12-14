d6cdd3e2ef8f16429956a7bdadf8f3352c04d5b0
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-14 15:21:06 +0500

    Loops Practice

--- Loops.js
+++ Loops.js
@@ -24,7 +24,10 @@ for (let i = 10; i > 0; i--){
     // if (i % 2 !== 0) console.log(i);
 };
 
-
+// ======================
+for ( let i = 1; i  <= 15; i++){
+    // console.log ( "No.",i, "abc xyz")
+}
 
 
 // WHILE Loop
@@ -40,22 +43,33 @@ while (i <=5){
 
 let j = 0;
 while (j <= 5){
-    // if (j % 2 !== 0) console.log('Hello While Odd Values', j)
+    if (j % 2 !== 0) console.log('Hello While Odd Values', j)
     j++;
 };
 
+let m = 1;
+while ( m <= 15){
 
+    // console.log ("No.", m , "Student");
+    m++;
+};
+
+let n = 10;
+while ( n <= 100){
+    if( n % 2 !== 1)console.log( 'Hello number', n )
+    n++;
+};
 
 // Do-While Loop
 let k = 0;
 do{
-    if (k % 2 !== 0) console.log('Hello do while loop' , k);
+    // if (k % 2 !== 0) console.log('Hello do while loop' , k);
     k++;
 } while(k <= 5);
 
 let l = 9;
 do {
-    if (l % 2 !== 0) console.log('Hello Do While Loop False', l)
+    // if (l % 2 !== 0) console.log('Hello Do While Loop False', l);
     l++;
 } while ( l <= 5);
 
