fbc45575891451411bb7f28085f7123598d75369
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-21 02:06:27 +0500

    Exercise Grade

--- ex even&odd.js
+++ ex even&odd.js
@@ -29,14 +29,14 @@ function showNumbers(limit){
 const array = [ '', 1, 2 , 0, undefined , 'SAQIB'];
 // console.log(coutnTruthy(array));
 
-function coutnTruthy(array){
-    let count = 0;
-    for ( let value of array )
-        if(value)
-        count++;
-        return count;
+// function coutnTruthy(array){
+//     let count = 0;
+//     for ( let value of array )
+//         if(value)
+//         count++;
+//         return count;
     
-};
+// };
 let movie ={
     releaseYear : 2020,
     title : 'a',
@@ -45,19 +45,19 @@ let movie ={
 
 };
 
-showProperties(movie);
+// showProperties(movie);
 
-function showProperties(obj){
+// function showProperties(obj){
 
-    for ( let key in obj )
-    if (typeof obj[key] === 'string')
-    console.log (key, obj[key]);
+//     for ( let key in obj )
+//     if (typeof obj[key] === 'string')
+//     console.log (key, obj[key]);
     
 
-    for (let prop in obj )
-    if (typeof obj[prop] !== 'string')
-    console.log( prop, obj[prop])
-};
+//     for (let prop in obj )
+//     if (typeof obj[prop] !== 'string')
+//     console.log( prop, obj[prop])
+// };
 
 
 
@@ -65,7 +65,7 @@ function showProperties(obj){
 // Sum of multiples of 3 & 5
 //==========================//
 
-console.log(sum(10));
+// console.log(sum(10));
 
 function sum(limit){
     let sum = 0;
@@ -79,15 +79,50 @@ function sum(limit){
     
 }
 
-console.log(diff(10))
+// console.log(diff(10))
 function diff (limit){
     let diff = 0;
 
     for (i = 0; i <= limit; i++)
-        if ( i % 3 === 0 || i % 5 === 0 ){
-            // console.log(i)
-            diff -= i;}
+        if ( i % 3 === 0 || i % 5 === 0 )
+        diff -= i;
     
     return diff;
 
+}
+
+
+
+// Exercise Grade 
+//==============//
+
+//1 - 59 = F
+// 60 - 69 = D
+// 70 - 79 = C
+// 80 - 89 = B
+// 90 - 100 = A
+
+
+const saqib = [99, 61, 99];
+
+console.log(calculateGrade(saqib));
+
+function calculateGrade(saqib) {
+    const average = calculateAverage(saqib)
+     console.log ( average );
+
+    if ( average < 60) return "F";
+    if ( average < 70) return "D";
+    if ( average < 80) return "C";
+    if ( average < 90) return "B";
+    return "A";
+
+}
+
+function calculateAverage(array){
+    let sum = 0;
+
+    for (let value of array)
+    sum += value;
+    return sum / array.length;
 }
