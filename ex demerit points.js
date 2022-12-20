67653eabcf3c4a7dc37db6d2d3420a5d2f49d4eb
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-20 21:44:05 +0500

    EVEN ODD Separation exercise

--- ex demerit points.js
+++ ex demerit points.js
@@ -1,25 +1,10 @@
+//DEMERIT POINTS Exercise 
+//=======================//
 // Speed Limit = 70
 // 5 -> 1 point
 // (Hint) Math.floor(1.3) => 1
 // 12 Points -> Licence Suspended
 
-
-// checkSpeed(890)
-// function checkSpeed(speed) {
-//     const speedLimit = 70;
-//     const kmPerPoint = 5;
-
-//     if (speed <= speedLimit) 
-//     console.log('Great :). You are driving under limit speed.');
-
-//     else{
-//         const points = Math.floor((speed - speedLimit) / kmPerPoint);
-        
-//         if (points >= 12) console.log('Your lisence has been suspended due to over limit.');
-//         else console.log( 'Points: ', points)
-//     }
-
-// }; 
 
 
 checkSpeed(90)
