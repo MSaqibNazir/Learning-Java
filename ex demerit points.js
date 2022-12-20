a85e31ba837c6c935486739ffa4260113c929fad
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-20 21:27:09 +0500

    Demerit Points Exercise

--- ex demerit points.js
+++ ex demerit points.js
@@ -22,18 +22,21 @@
 // }; 
 
 
-checkSpeed(123)
+checkSpeed(90)
 function checkSpeed(speed){
     const speedLimit = 70;
     const kmPerPoint = 5;
     
-    if( speed < speedLimit) console.log( 'Good' );
-    else {
-        let points = Math.floor(( speed - speedLimit ) / kmPerPoint);  
-        
-        if ( points >= 12) console.log ( 'Lisence suspended' )
-        else 
-            console.log ( 'Points:', points)
+    if( speed < speedLimit + kmPerPoint) {
+        console.log( 'Good' );
+        return;
     };
+
+    let points = Math.floor(( speed - speedLimit ) / kmPerPoint);  
+
+    if ( points >= 12)
+     console.log ( 'Lisence suspended' )
+    else 
+    console.log ( 'Points:', points)
 
 };
