ca4d4512059a8319b18b2f06058edfbd6de22529
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-26 17:25:26 +0500

    Iterating Arrays

---
+++ arrays2.js
@@ -0,0 +1,10 @@
+// Iterating an Array
+
+//  for of loop
+
+const numbers = [1,2,3,4]
+
+// for (let number of numbers)
+    // console.log(number) 
+
+numbers.forEach((number, index) => console.log(index, number ));
