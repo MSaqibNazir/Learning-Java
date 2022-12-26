07699e4976119d44e658c94907212a1b6dd07b52
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-26 19:23:21 +0500

    joining arrays

--- arrays2.js
+++ arrays2.js
@@ -7,4 +7,22 @@ const numbers = [1,2,3,4]
 // for (let number of numbers)
     // console.log(number) 
 
-numbers.forEach((number, index) => console.log(index, number ));
+// numbers.forEach((index, number) => console.log(index, number )); 
+
+// java ka apna method hai for each
+
+/////////////// =======================
+
+// Joining Arrays
+
+const alphabets = [1,2,3,4];
+
+const joined = alphabets.join(',');
+console.log (joined);
+
+const message = 'This is my first message.'
+const parts = message.split(' ');
+console.log(parts);
+
+const joinedParts = parts.join('-')
+console.log(joinedParts); // it isused for Url Slugs
