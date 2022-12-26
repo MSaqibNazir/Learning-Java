0fcf30392ca2d33c34a02a5b54286975abcb890b
Saqib Nazeer <saqibnazeer925@gmail.com>
2022-12-26 19:51:29 +0500

    sorting Arrays

--- arrays2.js
+++ arrays2.js
@@ -26,3 +26,33 @@ console.log(parts);
 
 const joinedParts = parts.join('-')
 console.log(joinedParts); // it isused for Url Slugs
+
+
+// Sorting Arrays
+const number = [2, 3, 1];
+
+number.sort()
+console.log(number);
+
+number.reverse()
+console.log(number);
+
+const courses = [
+    { id : 1, name : 'Node.js' },
+    { id : 2, name : 'Node.js'}
+];
+
+courses.sort( function (a, b){
+    // if a<b => -1
+    // if a>b => 1
+    // if a === b => 0
+
+    let nameA = a.name.toUpperCase();
+    let nameB = b.name.toUpperCase()
+
+    if (nameA < nameB) return -1
+    if (nameA > nameB) return 1
+    return 0;
+
+})
+console.log(courses)
