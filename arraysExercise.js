d38290a5314d0722b5b8f7ef29125db94927ce1e
Saqib Nazeer <saqibnazeer925@gmail.com>
2023-01-13 02:48:04 +0500

    Exercise5 - Count Occurrences

--- arraysExercise.js
+++ arraysExercise.js
@@ -58,7 +58,7 @@ const numbM = [1,2,3,4];
 
 const outPut = move(numbM, 2 , 1);
 
-console.log(outPut);
+// console.log(outPut);
 
 function move(array, index, offset){
     const position = index + offset
@@ -72,4 +72,37 @@ function move(array, index, offset){
     const element = outPut.splice(index,1)[0];
     outPut.splice (position, 0, element)
     return outPut;
-}
+};
+
+
+
+
+// Exercise5 - Count Occurrences
+//=============================================================
+ const numbC = [1, 2, 3, 4, 3, 2, 1, 1,2];
+
+const count = countOccurrences(numbC, 1 );
+
+console.log(count);
+
+function countOccurrences (array, searchElement){
+//     let counter = 0;
+
+//     for (let n of array)
+//     if( n == searchElement) counter++;
+    
+// return counter;
+// lets how to use reduce method to achive this same result
+
+
+// accumulator means the number times the searched element occured in array
+
+return array.reduce((accumulator, current) => {
+const occurrence = (current === searchElement) ? 1 : 0;
+// to check whats ahppening
+console.log(accumulator, current, searchElement);
+return accumulator + occurrence;
+}, 0);
+
+ };
+
