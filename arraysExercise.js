a45c47db5da46c2324cc0d2b5556274864957adb
Saqib Nazeer <saqibnazeer925@gmail.com>
2023-01-13 02:14:17 +0500

    Moving an element

--- arraysExercise.js
+++ arraysExercise.js
@@ -38,16 +38,38 @@ function includes(array, searchElement){
 
 const numbE = [ 1, 3, 5, 7, 1, 1 ];
 
+// console.log(except(numbE, [1,3]));
 
-console.log(except(numbE, [1,3]));
-
-function except ( array, excluded ){
+function except ( array, excludedValue ){
     const output = [];
 
     for ( let element of array )
     
-    if ( !excluded.includes(element) )
+    if ( !excludedValue.includes(element) )
         output.push(element);
     
     return output;
+};
+
+
+// Exercise4 - Moving an Element
+//=============================================================
+const numbM = [1,2,3,4];
+
+const outPut = move(numbM, 2 , 1);
+
+console.log(outPut);
+
+function move(array, index, offset){
+    const position = index + offset
+    if ( position >= array.length || position < 0){
+        console.error('Invalid Offset.');
+        return
+    }
+      
+    const outPut = [...array];
+    // first we have to delete that element and then add it
+    const element = outPut.splice(index,1)[0];
+    outPut.splice (position, 0, element)
+    return outPut;
 }
