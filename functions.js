393daa31ec6c1c8b027a811b4386e4b30291bf37
Saqib Nazeer <saqibnazeer925@gmail.com>
2023-01-15 18:13:29 +0500

    function exercise 2

--- functions.js
+++ functions.js
@@ -257,7 +257,7 @@ const blog = {
     showPosts () {
         this.posts.forEach(post => {
             console.log(this.title, post)
-        })
+        }) 
     }
 }
 
@@ -273,3 +273,17 @@ const blog = {
 // playAudio.bind({name : 'Mudassir'})();
 
 // playAudio()
+
+
+const number = [ 1, 2, 5, 3, 4, 5];
+
+const  count = countOccurence (number, 5)
+
+console.log(count);
+
+function countOccurence (array, searchedElement) {
+    return array.reduce (( accumulator, current ) => {
+        const occurence = (current === searchedElement) ? 1 : 0
+        return accumulator + occurence;
+    })
+}
