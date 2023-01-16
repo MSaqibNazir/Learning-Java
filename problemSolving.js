7ec8aebca09b058e0a324877194a2bf94bd4afbe
Saqib Nazeer <saqibnazeer925@gmail.com>
2023-01-16 08:22:18 +0500

    hackerRank migratory Bird

--- problemSolving.js
+++ problemSolving.js
@@ -61,30 +61,88 @@ function getTotalx(){
 // Given an array of integers and a positive integer k , determine 
 // the number of (i , j) pairs where i < j and ar[i] + ar[j]  is divisible by k.
 
-const ar= [1,3,2,6,1,2];
-const k = 3;
-let n = ar.length;
+// const ar= [1,3,2,6,1,2];
+// const k = 3;
+// let n = ar.length;
 
-// divisibleSumPairs(n, k, ar)
+// const prob = function divisibleSumPairs(n, k, ar) {
+//     var countOfPairs = 0
 
-// function divisibleSumPairs(n, k, ar) {
-//     let pairsCount = 0;
-    
-   
-    
+//     for( let j = 0; j < n - 1; j++){
+//         for (let i = j + 1; i < n ; i++)
+//             if((ar[i] + ar[j]) % k === 0)
+//                 countOfPairs++;
+// }
+// return countOfPairs;
+// }
 
+// console.log(prob())
+
+
+
+// Migratory Birds
+//=====================================================================
+//Given an array of bird sightings where every element represents 
+//a bird type id, determine the id of the most frequently sighted type. 
+//If more than 1 type has been spotted that maximum amount, return the 
+//smallest of their ids.
+
+
+// console.log('Mudassir bhai')
+
+//{ arr  = [1 ,2, 3, 4, 5, 4, 3, 2, 1, 3,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2, 4]
+
+// array = [0,0,0,0,0]
+
+//  a =1
+
+// function findGreater(arr, array){
+// for (i=0; i<arr.length ; i++){
+//     array[arr[i]-1]++
+// }
+
+// var maximumValue = array[0]
+
+
+// console.log("array",array)
+// for(i=1; i<5; i++){
+// console.log( maximumValue, array[i],)
+//     if(maximumValue < array[i]){
+//        console.log("CHECKKK",   maximumValue,array[i],)
+//         maximumValue == array[i+1]
+//        a = i
+//     }
+// }
+
+//     return a;
 // }
 
 
-const prob = function divisibleSumPairs(n, k, ar) {
-    var countOfPairs = 0
+// console.log(findGreater(arr, array))
 
-    for( let j = 0; j < n - 1; j++){
-        for (let i = j + 1; i < n ; i++)
-            if((ar[i] + ar[j]) % k === 0)
-                countOfPairs++;
-}
-return countOfPairs;
+// };
+
+
+// console.log('Saqib bhai')
+
+
+arr = [5, 5, 2, 2, 3];
+
+function migratoryBirds(arr){
+    arr = arr.sort();
+    let ans = arr[0];
+    let maxOccur = 1,
+        counter = 0;
+    
+    for (let i = 0; i < arr.length; i++) {
+        counter = (arr[i] === arr[i-1]) ? counter + 1 : 0;
+        if (counter > maxOccur) {
+            ans = arr[i];
+            maxOccur = counter;
+        }
+    }
+
+    return ans;
 }
 
-console.log(prob)
+console.log(migratoryBirds(arr));
