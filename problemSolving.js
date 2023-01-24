887815db3c0396cb3b267bcf8ca6b97eb2965afe
Saqib Nazeer <saqibnazeer925@gmail.com>
2023-01-25 03:18:35 +0500

    HackerRank Ex - Big Sorting

--- problemSolving.js
+++ problemSolving.js
@@ -682,84 +682,17 @@ function countingValleys(steps, path) {
 
 // console.log(tr);
 
-// HackerRank Ex - Matrix Layer Rotation
-// =====================================
+// HackerRank Ex - Big Sorting
+//============================
 
-
-// rows = m 
-// columns = n
-// rotation = r
-
-
-let matrix = [
-              [ 1,  2,  3,  4],                      //index
-              [ 5,  6,  7,  8], // x * y..... in array [y]  [x]
-              [ 9, 10, 11, 12], 
-              [13, 14, 15, 16]
-            ];
-let r = 1;
-
-console.log(matrixRotation(matrix, r));
-
-
-function matrixRotation(matrix, r) {
-
-  const row = matrix.length;
-  const col = matrix[0].length;
-  const layer = Math.min(row, col) / 2;
-  const split = [];
-
-
-  for (let i = 0; i < layer; i++) {
-    const temp = [];
-    for (let j = i; j < col - i; j++) {
-      temp.push(matrix[i][j]);
-    }
-    for (let j = i + 1; j < row - i; j++) {
-      temp.push(matrix[j][col - i - 1]);
-    }
-    for (let j = col - i - 2; j >= i; j--) {
-      temp.push(matrix[row - i - 1][j]);
-    }
-    for (let j = row - i - 2; j > i; j--) {
-      temp.push(matrix[j][i]);
-    }
-    split.push(temp);
-  }
-  // rotate
-  for (let i = 0; i < layer; i++) {
-    const temp = [];
-    for (let j = 0; j < split[i].length; j++) {
-      temp.push(split[i][(j + r) % split[i].length]);
-      console.log((j + r) % split[i].length);
-      
-    }
-    // split[i] = temp;
-  }
-// console.log(split);
-
-//   // merge
-//   for (let i = 0; i < layer; i++) {
-//     let index = 0;
-//     for (let j = i; j < col - i; j++) {
-//       matrix[i][j] = split[i][index++];
-//     }
-//     for (let j = i + 1; j < row - i; j++) {
-//       matrix[j][col - i - 1] = split[i][index++];
-//     }
-//     for (let j = col - i - 2; j >= i; j--) {
-//       matrix[row - i - 1][j] = split[i][index++];
-//     }
-//     for (let j = row - i - 2; j > i; j--) {
-//       matrix[j][i] = split[i][index++];
-//     }
-//   }
-
-//   for (let i = 0; i < row; i++) {
-//     let str = "";
-//     for (let j = 0; j < col; j++) {
-//       str += matrix[i][j] + " ";
-//     }
-//     console.log(str);
-//   }
+let unsorted = ['1', '3', '10', '3', '5' ];
+let forSort = []
+for ( let i = 0; i < unsorted.length; i++) {
+    forSort.push(+unsorted[i]);
 }
+
+forSort.sort((a,b) => a - b)
+let final = forSort.join(',')
+let fullFinal = final.split(',')
+
+console.log(fullFinal);
